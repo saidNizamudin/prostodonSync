c0dc8d1a0eda7c0e44fade2366f63882bcd2c1e3
saidNizamudin <saidnizamudin@gmail.com>
2024-12-15 22:20:12 +0700

    add lightbulb logic into admin

--- app/admin/[scheduleId]/page.tsx
+++ app/admin/[scheduleId]/page.tsx
@@ -12,6 +12,7 @@ import {
   Lightbulb,
   Loader,
   PlusCircle,
+  StickyNote,
   Trash,
 } from "lucide-react";
 import { Button } from "@/components/button";
@@ -31,11 +32,18 @@ import toast from "react-hot-toast";
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
 } from "@/components/dialog";
+import { format } from "date-fns";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/popover";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/tooltip";
+import { useMediaQuery } from "react-responsive";
 
 interface CategoryType {
   id?: string;
@@ -44,9 +52,16 @@ interface CategoryType {
   slot?: number;
   desc?: string;
   scheduleId?: string;
+  participants?: {
+    id: string;
+    name: string;
+    notes?: string;
+    createdAt: string;
+  }[];
 }
 
 export default function CategoryAdminPage() {
+  const isMobile = useMediaQuery({ query: "(max-width: 500px)" });
   const [isCreateMode, setIsCreateMode] = useState(false);
   const [newCategory, setNewCategory] = useState<CategoryType>({
     title: "",
@@ -210,50 +225,174 @@ export default function CategoryAdminPage() {
         </Button>
       </div>
       <div className="flex flex-wrap w-full gap-5 items-center justify-center">
-        {data?.map((category) => (
-          <div
-            key={category.id}
-            className="flex flex-col border border-gray-300 rounded-md p-5 w-[500px] h-[200px]"
-          >
-            <span className="text-xl font-semibold leading-tight">
-              {category.title}
-            </span>
-            <span className="flex items-center gap-2 text-base font-semibold leading-none">
-              {`INSTRUKTUR: ${category.instructor}`}
-            </span>
-            <span className="text-sm text-black/80 mb-auto overflow-hidden text-ellipsis line-clamp-3">
-              {category.desc}
-            </span>
-            <div className="flex justify-between mt-5 items-center gap-2">
-              <Badge className="w-max mt-auto">
-                {category.slot} slots open
-              </Badge>
-              <div className="flex gap-2">
-                <Button size={"icon"}>
-                  <Lightbulb size={20} />
-                </Button>
-                <Button
-                  size={"icon"}
-                  onClick={(e) => {
-                    setIsEditMode(true);
-                    setSelectCategory(category);
-                  }}
-                >
-                  <Edit2Icon size={20} />
-                </Button>
-                <Button
-                  size={"icon"}
-                  variant={"destructive"}
-                  onClick={(e) => {
-                    handleDelete(category.id);
-                  }}
-                >
-                  <Trash size={20} />
-                </Button>
+        {data?.map((category) => {
+          if (
+            !category ||
+            !category.id ||
+            !category.title ||
+            !category.instructor ||
+            !category.slot
+          ) {
+            return (
+              <div
+                key={category.id}
+                className="flex border border-gray-300 rounded-md p-5 w-[300px] h-[200px] justify-center items-center cursor-not-allowed shadow-lg"
+              >
+                <span className="text-xl font-semibold leading-tight">
+                  Something went wrong with this category
+                </span>
+              </div>
+            );
+          }
+          const slotLeft = category.slot - (category.participants?.length || 0);
+
+          return (
+            <div
+              key={category.id}
+              className="flex flex-col border border-gray-300 rounded-md p-5 w-[500px] h-[200px]"
+            >
+              <span className="text-xl font-semibold leading-tight">
+                {category.title}
+              </span>
+              <span className="flex items-center gap-2 text-base font-semibold leading-none">
+                {`INSTRUKTUR: ${category.instructor}`}
+              </span>
+              <span className="text-sm text-black/80 mb-auto overflow-hidden text-ellipsis line-clamp-3">
+                {category.desc}
+              </span>
+              <div className="flex justify-between mt-5 items-center gap-2">
+                <div className="flex flex-col gap-2">
+                  <Badge className="w-max mt-auto">
+                    {category.slot} slots open
+                  </Badge>
+                  <Badge
+                    className="w-max mt-auto"
+                    variant={slotLeft > 0 ? "success" : "destructive"}
+                  >
+                    {slotLeft > 0 ? `${slotLeft} slots left` : "Full"}
+                  </Badge>
+                </div>
+                <div className="flex gap-2">
+                  <Button
+                    size={"icon"}
+                    variant={"destructive"}
+                    onClick={(e) => {
+                      handleDelete(category.id);
+                    }}
+                  >
+                    <Trash size={20} />
+                  </Button>
+                  <Button
+                    size={"icon"}
+                    onClick={(e) => {
+                      setIsEditMode(true);
+                      setSelectCategory(category);
+                    }}
+                  >
+                    <Edit2Icon size={20} />
+                  </Button>
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <Button size={"icon"}>
+                        <Lightbulb size={20} />
+                      </Button>
+                    </PopoverTrigger>
+                    <PopoverContent
+                      className="w-[400px] p-5 max-h-[400px] overflow-y-auto max-[500px]:w-[250px]"
+                      align="end"
+                    >
+                      {category.participants?.length ? (
+                        <div className="flex flex-col gap-2">
+                          <span className="text-lg font-semibold">
+                            Participants
+                          </span>
+                          <div className="flex flex-col gap-2">
+                            {category.participants
+                              .slice(0, category.slot)
+                              .map((participant) => (
+                                <div
+                                  className="flex flex-col items-start"
+                                  key={participant.id}
+                                >
+                                  {isMobile ? (
+                                    <span className="text-sm">
+                                      {participant.name}
+                                    </span>
+                                  ) : (
+                                    <div className="flex items-center gap-1">
+                                      <span className="text-sm text-nowrap">
+                                        {participant.name}
+                                      </span>
+                                      {participant.notes && (
+                                        <TooltipProvider delayDuration={0}>
+                                          <Tooltip>
+                                            <TooltipTrigger>
+                                              <StickyNote
+                                                size={14}
+                                                color="grey"
+                                              />
+                                            </TooltipTrigger>
+                                            <TooltipContent>
+                                              {participant.notes}
+                                            </TooltipContent>
+                                          </Tooltip>
+                                        </TooltipProvider>
+                                      )}
+                                    </div>
+                                  )}
+                                  <span className="text-xs text-black/60">
+                                    {format(
+                                      new Date(participant.createdAt),
+                                      "dd/MM/yyyy HH:mm:ss"
+                                    )}
+                                  </span>
+                                </div>
+                              ))}
+                            {category.participants.length > category.slot && (
+                              <>
+                                <hr
+                                  className="my-2 h-2 border-black"
+                                  style={{
+                                    marginInline: -20,
+                                  }}
+                                />
+                                <span className="text-xs text-destructive leading-none w-full">
+                                  * more participant(s) on waiting list
+                                </span>
+                                {category.participants
+                                  .slice(category.slot)
+                                  .map((participant) => (
+                                    <div
+                                      className="flex flex-col items-start"
+                                      key={participant.id}
+                                    >
+                                      <span className="text-sm">
+                                        {participant.name}
+                                      </span>
+                                      <span className="text-xs text-black/60">
+                                        {format(
+                                          new Date(participant.createdAt),
+                                          "dd/MM/yyyy HH:mm:ss"
+                                        )}
+                                      </span>
+                                    </div>
+                                  ))}
+                              </>
+                            )}
+                          </div>
+                        </div>
+                      ) : (
+                        <span className="text-lg font-semibold">
+                          No participants yet
+                        </span>
+                      )}
+                    </PopoverContent>
+                  </Popover>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
       <Sheet open={isCreateMode} onOpenChange={() => setIsCreateMode(false)}>
         <SheetContent className="min-w-[600px] max-[500px]:min-w-[300px]">
